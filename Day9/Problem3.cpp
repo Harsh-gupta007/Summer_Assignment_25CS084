44a49bb8461f590bddc6deefe034ea041ca6d946
Harsh-gupta007 <harsh.gupta2025@glbajajgroup.org>
2026-06-13 19:51:50 +0530

    Day9 uploaded

---
+++ Day9/Problem3.cpp
@@ -0,0 +1,30 @@
+// Write a program to Print repeated character pattern.
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {  char ch = 'A'+ i - 1;
+        
+       for (int j= 1; j <=  i; j++)
+       {
+        cout << ch;
+       }
+       cout << endl;
+    }
+    
+
+
+    return 0;
+}
---
+++ Day9/Problem4.cpp
@@ -0,0 +1,35 @@
+// Write a program to Print hollow square pattern.
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+
+    cout << "Enter the size of the square: ";
+    cin >> size;
+
+    for (int i = 1; i <= size; i++)
+    {
+        for (int j = 1; j <= size; j++)
+        {
+          if(i == 1|| i == size || j == 1 || j == size){
+            cout << "*";
+          }else{
+            cout << " ";
+          }
+        }
+        cout << endl;
+        
+    }
+    
+
+
+    return 0;
+}
