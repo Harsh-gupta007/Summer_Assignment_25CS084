8b5d96782855325af159fb3ad1ac2016ba01bfc5
Harsh-gupta007 <harsh.gupta2025@glbajajgroup.org>
2026-06-13 19:51:29 +0530

    Day8 uploaded

---
+++ Day8/Problem3.cpp
@@ -0,0 +1,31 @@
+// Write a program to Print character triangle.
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
+#include<iostream>
+using namespace std;
+
+int  main(){
+
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (char ch = 'A'; ch < 'A'+ i; ch++)
+        {
+            cout << ch;
+        }
+       cout << endl;   
+    }
+    
+
+
+    return 0;
+}
+
+            
